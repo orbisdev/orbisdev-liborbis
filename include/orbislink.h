d63cff250fe2caceb852d1ab691b02e2ec6cd00a
masterzorag <masterzorag@gmail.com>
2020-10-30 23:10:30 +0100

    orbislink: review
        - use cleanup()
        - convert TAB to 4 spaces

--- include/orbislink.h
+++ include/orbislink.h
@@ -1,7 +1,7 @@
 /*
 #  ____   ____    ____         ___ ____   ____ _     _
 # |    |  ____>   ____>  |    |        | <____  \   /
-# |____| |    \   ____>  | ___|    ____| <____   \_/	ORBISDEV Open Source Project.
+# |____| |    \   ____>  | ___|    ____| <____   \_/   ORBISDEV Open Source Project.
 #------------------------------------------------------------------------------------
 # Copyright 2010-2020, orbisdev - http://orbisdev.github.io
 # Licenced under MIT license
@@ -21,15 +21,17 @@
 
 typedef struct OrbisGlobalConf
 {
-	void *conf; //deprecated
-	OrbisPadConfig *confPad;
-	OrbisAudioConfig *confAudio;
-	OrbisKeyboardConfig *confKeyboard;
-	void *confLink; //deprecated
-	int orbisLinkFlag;
-	debugNetConfiguration *confDebug;
-	OrbisNfsConfig *confNfs;
+    void *conf; //deprecated
+    OrbisPadConfig *confPad;
+    OrbisAudioConfig *confAudio;
+    OrbisKeyboardConfig *confKeyboard;
+    void *confLink; //deprecated
+    int orbisLinkFlag;
+    debugNetConfiguration *confDebug;
+    OrbisNfsConfig *confNfs;
 }OrbisGlobalConf;
+
+typedef void module_patch_cb_t(void *arg, uint8_t *base, uint64_t size);
 
 
 #ifdef __cplusplus
@@ -42,9 +44,11 @@ int initOrbisLinkAppInternal(void);
 int initOrbisLinkAppVanilla(void);
 int initOrbisLinkAppVanillaGl(void);
 int initOrbisLinkAppVanillaGlMSX(void);
-OrbisGlobalConf * orbisLinkGetGlobalConf(void);
+OrbisGlobalConf *orbisLinkGetGlobalConf(void);
 void finishOrbisLinkApp(void);
 
+bool patch_module(const char *name, module_patch_cb_t *cb, void *arg, int level);
+int  loadModulesVanilla(void);
 
 #ifdef __cplusplus
 }
