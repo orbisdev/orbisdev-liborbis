239087a06ffbf6242da50396efe2a2a1767e4993
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2022-01-01 19:23:47 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ SUBDIRS="orbisdev-utils orbisdev-libkernelUtil orbisdev-libdebugnet orbisdev-lib
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Download the source code.
-## Download the source code.
 for dir in $SUBDIRS
 do 
 	echo $dir
@@ -20,3 +19,4 @@ do
 	make --quiet -j $PROC_NR clean || { exit 1; } 
 	cd ..
 done
+
