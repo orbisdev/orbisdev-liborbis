0a29062e85f84be42857bdc5dcfe4fd22b008d37
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2022-01-01 19:22:19 +0100

    build and install each library

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 SUBDIRS="orbisdev-utils orbisdev-libkernelUtil orbisdev-libdebugnet orbisdev-libz orbisdev-liborbisPad orbisdev-liborbisAudio orbisdev-libmod orbisdev-liborbisKeyboard orbisdev-liborbisNfs orbisdev-libSQLite orbisdev-liborbisLink"
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Download the source code.
 ## Download the source code.
@@ -12,6 +13,10 @@ do
 		cd $dir && 
 			git pull && git fetch origin &&
 			git reset --hard origin/master || exit 1
-		fi
+	fi
+	make --quiet -j $PROC_NR clean || { exit 1; }
+	make --quiet -j $PROC_NR || { exit 1; }
+	make --quiet -j $PROC_NR install || { exit 1; }
+	make --quiet -j $PROC_NR clean || { exit 1; } 
 	cd ..
 done
