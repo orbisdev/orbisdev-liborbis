e500a5bbc3b490ab412521f24fdcdd97c95606f2
masterzorag <masterzorag@gmail.com>
2022-01-22 16:02:35 +0100

    CI: pull from different branch

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 SUBDIRS="orbisdev-utils orbisdev-libkernelUtil orbisdev-libdebugnet orbisdev-libz orbisdev-liborbisPad orbisdev-liborbisAudio orbisdev-liborbisNfs orbisdev-liborbisKeyboard orbisdev-libmod orbisdev-libSQLite orbisdev-liborbisLink"
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
+
+## Fetch the main headers
+git clone https://github.com/orbisdev/orbisdev-headers --depth=1
+cp -a orbisdev-headers/include/* $ORBISDEV/usr/include/
+
 ## Download the source code.
 for dir in $SUBDIRS
 do 
